18b114261878f1ce51b18d21a85b295ce9f3df9d
Daniel Giao <daniel.giao@icloud.com>
2025-01-25 13:08:42 -0500

    Added findAllClubs in service class

---
+++ src/main/java/service/ClubService.java
@@ -0,0 +1,9 @@
+package service;
+
+import com.runningclub.web.dto.ClubDto;
+
+import java.util.List;
+
+public interface ClubService {
+    List<ClubDto> findAllClubs();
+}
---
+++ src/main/java/service/impl/ClubServiceImpl.java
@@ -0,0 +1,36 @@
+package service.impl;
+
+import com.runningclub.web.dto.ClubDto;
+import com.runningclub.web.models.Club;
+import com.runningclub.web.repository.ClubRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import service.ClubService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ClubServiceImpl implements ClubService {
+    private ClubRepository clubRepository;
+
+    @Autowired
+    public ClubServiceImpl(ClubRepository clubRepository) {
+        this.clubRepository = clubRepository;
+    }
+    @Override
+    public List<ClubDto> findAllClubs() {
+        List<Club> clubs = clubRepository.findAll();
+        return clubs.stream().map((club) -> mapToClubDto(club)).collect(Collectors.toList());
+    }
+
+    private ClubDto mapToClubDto(Club club) {
+        ClubDto clubDto = ClubDto.builder()
+                .id(club.getId())
+                .title(club.getTitle())
+                .photoUrl(club.getPhotoUrl())
+                .content(club.getContent())
+                .createdOn(club.getCreatedOn())
+                .updatedOn(club.getUpdatedOn())
+                .build();
+        return clubDto;
+    }
+}
