4d87bd68ff6fcce833e9e43469e3e6abcd1fb124
Daniel Giao <daniel.giao@icloud.com>
2025-01-27 18:55:01 -0500

    Added create endpoint and form

--- src/main/java/com/runningclub/web/service/ClubService.java
+++ src/main/java/com/runningclub/web/service/ClubService.java
@@ -1,9 +1,11 @@
 package com.runningclub.web.service;
 
 import com.runningclub.web.dto.ClubDto;
+import com.runningclub.web.models.Club;
 
 import java.util.List;
 
 public interface ClubService {
     List<ClubDto> findAllClubs();
+    Club saveClub(Club club);
 }
