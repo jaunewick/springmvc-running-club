b32e7458185a3b24c5f3a3a6be687aa4ea3be207
Daniel Giao <daniel.giao@icloud.com>
2025-01-30 21:18:13 -0500

    Added validation for form inputs

--- src/main/java/com/runningclub/web/controller/ClubController.java
+++ src/main/java/com/runningclub/web/controller/ClubController.java
@@ -6,11 +6,13 @@ import com.runningclub.web.service.ClubService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -50,7 +52,12 @@ public class ClubController {
     }
 
     @PostMapping("/clubs/{clubId}/edit")
-    public String updateClub(@PathVariable("clubId") long clubId, @ModelAttribute("club") ClubDto club) {
+    public String updateClub(@PathVariable("clubId") long clubId,
+                             @Valid @ModelAttribute("club") ClubDto club,
+                             BindingResult result) {
+        if (result.hasErrors()) {
+            return "clubs-edit";
+        }
         club.setId(clubId);
         clubService.updateClub(club);
         return "redirect:/clubs";
