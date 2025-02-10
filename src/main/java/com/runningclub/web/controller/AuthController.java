81630e10cf1cafc4d39c8d426b6a9acb91e3e91b
Daniel Giao <daniel.giao@icloud.com>
2025-02-09 23:56:09 -0500

    Added get register endpoint and register view

--- src/main/java/com/runningclub/web/controller/AuthController.java
+++ src/main/java/com/runningclub/web/controller/AuthController.java
@@ -1,4 +1,21 @@
 package com.runningclub.web.controller;
 
+import com.runningclub.web.dto.RegistrationDto;
+import com.runningclub.web.service.UserService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class AuthController {
+    private UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/register")
+    public String getRegisterForm(Model model) {
+        RegistrationDto user = new RegistrationDto();
+        model.addAttribute("user", user);
+        return "register";
+    }
 }
