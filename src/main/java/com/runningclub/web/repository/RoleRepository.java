913bee5fb7115027b8ce2ad878fa14113ad2e793
Daniel Giao <daniel.giao@icloud.com>
2025-02-09 23:19:13 -0500

    Added User and Role repositories

---
+++ src/main/java/com/runningclub/web/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.runningclub.web.repository;
+
+import com.runningclub.web.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
---
+++ src/main/java/com/runningclub/web/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.runningclub.web.repository;
+
+import com.runningclub.web.models.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByEmail(String email);
+    UserEntity findByUsername(String username);
+}
