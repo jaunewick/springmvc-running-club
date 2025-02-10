326f78a6693fe4c1e68435ef8c85b17372ed2dd7
Daniel Giao <daniel.giao@icloud.com>
2025-02-09 23:31:08 -0500

    Added UserService and UserServiceImpl

--- src/main/java/com/runningclub/web/service/UserService.java
+++ src/main/java/com/runningclub/web/service/UserService.java
@@ -1,4 +1,7 @@
 package com.runningclub.web.service;
 
+import com.runningclub.web.dto.RegistrationDto;
+
 public interface UserService {
+    void saveUser(RegistrationDto registrationDto);
 }
--- src/main/java/com/runningclub/web/service/impl/UserServiceImpl.java
+++ src/main/java/com/runningclub/web/service/impl/UserServiceImpl.java
@@ -1,4 +1,36 @@
 package com.runningclub.web.service.impl;
 
-public class UserServiceImpl {
+import com.runningclub.web.dto.RegistrationDto;
+import com.runningclub.web.models.Role;
+import com.runningclub.web.models.UserEntity;
+import com.runningclub.web.repository.RoleRepository;
+import com.runningclub.web.repository.UserRepository;
+import com.runningclub.web.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public void saveUser(RegistrationDto registrationDto) {
+        UserEntity user = new UserEntity();
+        user.setUsername(registrationDto.getUsername());
+        user.setEmail(registrationDto.getEmail());
+        user.setPassword(registrationDto.getPassword());
+        Role role = roleRepository.findByName("USER");
+        user.setRoles(Arrays.asList(role));
+        userRepository.save(user);
+    }
 }
