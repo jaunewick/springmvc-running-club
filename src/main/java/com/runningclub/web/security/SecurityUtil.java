64a193447a94858be3524b0ccab63cf79a2b2a07
Daniel Giao <daniel.giao@icloud.com>
2025-02-17 17:11:31 -0500

    Refactored SecurityUtil

--- src/main/java/com/runningclub/web/security/SecurityUtil.java
+++ src/main/java/com/runningclub/web/security/SecurityUtil.java
@@ -1,13 +1,15 @@
 package com.runningclub.web.security;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import org.springframework.security.core.userdetails.UserDetails;
 public class SecurityUtil {
-    public static UserDetails getSessionUser() {
-        Object user = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        if (user != null) {
-            return (UserDetails)user;
+    public static String getSessionUser() {
+        Authentication authentication  = SecurityContextHolder.getContext().getAuthentication();
+        if (!(authentication instanceof AnonymousAuthenticationToken)) {
+            String currentUserName = authentication.getName();
+            return currentUserName;
         }
         return null;
     }
